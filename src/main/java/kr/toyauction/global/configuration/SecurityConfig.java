e4c38e9ced7219c61720847fc55b66d8209cdcf6
gh <gh77@miraeit.net>
2022-07-27 19:48:36 +0900

    feat : AUC-137 권한 없을때 사용 할 핸들러 시큐리티에 등록

--- src/main/java/kr/toyauction/global/configuration/SecurityConfig.java
+++ src/main/java/kr/toyauction/global/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package kr.toyauction.global.configuration;
 
 import kr.toyauction.domain.member.handler.OAuth2SuccessHandler;
 import kr.toyauction.domain.member.service.OAuth2MemberService;
+import kr.toyauction.global.token.JwtAccessDeniedHandler;
 import kr.toyauction.global.token.JwtAuthenticationEntryPoint;
 import kr.toyauction.global.token.JwtFilter;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +17,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
     private final OAuth2MemberService oAuth2MemberService;
     private final OAuth2SuccessHandler successHandler;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private final JwtFilter jwtFilter;
 
     @Override
@@ -36,6 +39,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
 
                 .and()
                 .authorizeRequests()
