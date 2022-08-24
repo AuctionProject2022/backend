b3404a8c21d37421216d1ebffb170a9a50e12538
gh <gh77@miraeit.net>
2022-08-24 20:20:55 +0900

    chore : cors 전체 허용 프론트엔드의 개발 편의상 요청으로 9월2일까지 허용

--- src/main/java/kr/toyauction/global/configuration/SecurityConfiguration.java
+++ src/main/java/kr/toyauction/global/configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import kr.toyauction.global.token.JwtAccessDeniedHandler;
 import kr.toyauction.global.token.JwtAuthenticationEntryPoint;
 import kr.toyauction.global.token.JwtFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,6 +15,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -55,5 +61,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .userService(oAuth2MemberService);
 
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+    //Cors 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","OPTIONS","DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
