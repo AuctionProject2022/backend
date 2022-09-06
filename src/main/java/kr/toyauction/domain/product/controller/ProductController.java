df0e4326ab7f355436105e3b4900907dd4290d01
hjyang <diva57855@gmail.com>
2022-09-06 23:26:46 +0900

    AUC-101 상품이름 자동 검색 기능

--- src/main/java/kr/toyauction/domain/product/controller/ProductController.java
+++ src/main/java/kr/toyauction/domain/product/controller/ProductController.java
@@ -20,6 +20,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -57,57 +60,9 @@ public class ProductController {
 	}
 
 	@GetMapping(value = Url.PRODUCT  + "/autocomplete", produces = MediaType.APPLICATION_JSON_VALUE)
-	public String getAutoComplete(@RequestParam String prductName) {
-		String result = "{\n" +
-				"  \"success\": \"true\",\n" +
-				"  \"data\": {\n" +
-				"    \"size\": \"10\",\n" +
-				"    \"content\": [\n" +
-				"      {\n" +
-				"        \"productId\": 1,\n" +
-				"        \"productName\": \"Nike Air Zoom Iorem lorem lorem Pegasus 36 Miami\"\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"productId\": 2,\n" +
-				"        \"productName\": \"Zoom Iorem Pegasus 75 Nike Air\"\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"productId\": 3,\n" +
-				"        \"productName\": \"Zoom Nike Iorem Pegasus 75  Air\"\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"productId\": 4,\n" +
-				"        \"productName\": \"Zoom Iorem Pegasus 75 Air Nike\"\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"productId\": 5,\n" +
-				"        \"productName\": \"Zoom Iorem Pegasus 75  Air Nike Iorem Pegasus\"\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"productId\": 6,\n" +
-				"        \"productName\": \"나이키 에어 Nike Air\"\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"productId\": 7,\n" +
-				"        \"productName\": \"줌 나이키 Zoom Iorem Pegasus 75 Nike Air\"\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"productId\": 8,\n" +
-				"        \"productName\": \"Zoom Iorem Pegasus 75 Nike Air 나이키\"\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"productId\": 9,\n" +
-				"        \"productName\": \"Zoom 나이키 에어 Iorem Pegasus 75 Nike Air\"\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"productId\": 10,\n" +
-				"        \"productName\": \"Nike 나이키 에어\"\n" +
-				"      }\n" +
-				"    ]\n" +
-				"  }\n" +
-				"}";
-
-		return result;
+	public SuccessResponse<List<ProductAutoCompleteResponse>> getAutoComplete(@RequestParam String productName) {
+		List<Product> productList = productService.getAutoCompleteProduct(productName);
+		return SuccessResponseHelper.success(productList.stream().map(ProductAutoCompleteResponse::new).collect(Collectors.toList()));
 	}
 
 	@GetMapping(value = Url.PRODUCT, produces = MediaType.APPLICATION_JSON_VALUE)
--- src/main/java/kr/toyauction/domain/product/repository/ProductRepository.java
+++ src/main/java/kr/toyauction/domain/product/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package kr.toyauction.domain.product.repository;
 import kr.toyauction.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import java.util.List;
 
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findTop10ByProductNameContainsOrderByIdDesc(String productName);
 }
