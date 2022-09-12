56746a8a76d399dfb9a7f0716cd8ea96f9eb6f8e
sommoong <101040781+god-joo@users.noreply.github.com>
2022-09-13 00:19:12 +0900

    Revert "Auc 181 상품조회 API문서 수정"

--- src/main/java/kr/toyauction/domain/product/dto/BidPostResponse.java
+++ src/main/java/kr/toyauction/domain/product/dto/BidPostResponse.java
@@ -2,7 +2,6 @@ package kr.toyauction.domain.product.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
 import kr.toyauction.domain.product.entity.Bid;
-import kr.toyauction.global.dto.BaseResponse;
 import kr.toyauction.global.entity.BaseEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,15 +11,19 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-public class BidPostResponse{
+public class BidPostResponse extends BaseEntity {
 
     private Long bidId;
     private Integer bidPrice;
     private LocalDateTime createDatetime;
+    private LocalDateTime updateDatetime;
+    protected boolean enabled;
 
     public BidPostResponse(final Bid bid) {
         this.bidId = bid.getId();
         this.bidPrice = bid.getBidPrice();
         this.createDatetime = bid.getCreateDatetime();
+        this.updateDatetime = bid.getUpdateDatetime();
+        this.enabled = bid.isEnabled();
     }
 }
